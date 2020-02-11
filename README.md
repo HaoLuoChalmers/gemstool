2c82e2f6534f959c8cdb00d50388e0fd5901640e
lhao <lhao@chalmers.se>
2020-02-11 13:48:30 +0100

    doc : update read me

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # gemstool
+some small tools to build GEMs and other metabolic model
+
+upload update in Terminal:
+
+    python setup.py sdist
+    twine upload dist/*
+
+username: bioluohao  
+pw: l*4********
+
--- setup.py
+++ setup.py
@@ -8,3 +8,26 @@
 :returns: 
 :rtype: 
 """
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gemstool",
+    version="0.0.1",
+    author="Hao Luo",
+    author_email="bioluohao@outlook.com",
+    description="some small tools for my modeling work",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HaoLuoChalmers/gemstool",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
