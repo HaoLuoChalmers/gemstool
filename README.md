6ee40e25d5e4768ace26ce17f373bf60192b3513
lhao <lhao@chalmers.se>
2020-02-11 16:29:09 +0100

    doc :

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
-# gemstool
+# GEMstool
+
 some small tools to build GEMs and other metabolic model
 
-upload update in Terminal:
+## install :
+    pip install gemstool
 
+## upload update in Terminal:
     python setup.py sdist
     twine upload dist/*
 
 username: bioluohao  
 pw: l*4********
 
+## requriment:
+    pip install cobra
--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@
 :returns: 
 :rtype: 
 """
+from __future__ import absolute_import
 
-import setuptools
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="gemstool",
     version="0.0.1",
     author="Hao Luo",
@@ -23,7 +24,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/HaoLuoChalmers/gemstool",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         # "License :: OSI Approved :: MIT License",
